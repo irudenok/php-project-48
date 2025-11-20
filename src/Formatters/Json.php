ad8db9f753601e54617b3846f3cfde47d3068b62
Ilia <rudenok.it@gmail.com>
2025-11-20 08:34:37 +0300

    Small fixes

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -4,8 +4,8 @@ namespace Differ\Formatters\Json;
 
 function format(array $diffTree): string
 {
-    $options = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT;
-    $encoded = json_encode($diffTree, $options);
-
-    return $encoded;
+    return json_encode(
+        $diffTree,
+        JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR
+    );
 }
